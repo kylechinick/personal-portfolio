d9608ed58e396dd90c96532936f9cee35d057cf6
Kyle Chinick <kylechinick@icloud.com>
2022-03-07 13:57:18 -0800

    Refactored projectCard js into separate functions

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,21 +1,29 @@
-const projectButtons = [
-  ...document.querySelectorAll('.project-card .learn-more-button')
-];
-
-for (button of projectButtons) {
-  button.addEventListener('click', function () {
-    const projectDetailsCard = this.previousElementSibling;
-    projectDetailsCard.classList.remove('hidden');
-    const closeProjectDetailsCardButton = projectDetailsCard.firstElementChild;
-    closeProjectDetailsCardButton.addEventListener('click', function () {
-      projectDetailsCard.classList.add('hidden');
-    });
-    document.addEventListener('keydown', function (key) {
-      if (key.code === 'Escape') {
-        projectDetailsCard.classList.add('hidden');
-      } else {
-        return;
-      }
-    });
+const projectCards = document.querySelectorAll(
+  '.project-card .learn-more-button'
+);
+for (const projectCard of projectCards) {
+  projectCard.addEventListener('click', function () {
+    const projectDetailsWindow = this.previousElementSibling;
+    projectDetailsWindow.classList.remove('hidden');
   });
 }
+
+const projectDetailsCloseButtons = document.querySelectorAll(
+  '.project-summary__close-icon'
+);
+for (const projectDetailsCloseButton of projectDetailsCloseButtons) {
+  projectDetailsCloseButton.addEventListener('click', function () {
+    this.parentElement.classList.add('hidden');
+  });
+}
+
+document.addEventListener('keydown', function (key) {
+  if (key.code === 'Escape') {
+    const projectSummary = document.querySelectorAll('.project-summary');
+    projectSummary.forEach(function (summary) {
+      if (!summary.classList.contains('hidden')) {
+        summary.classList.add('hidden');
+      }
+    });
+  }
+});
